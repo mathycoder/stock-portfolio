0b4b10f00bcb09a0539fdea08a753f9abba673ed
Adam Sarli <adam.sarli@gmail.com>
2020-02-13 13:14:54 -0500

    created User model and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   get '*path', to: "static_pages#index", constraints: ->(request) do
     !request.xhr? && request.format.html?
   end
+
+  get '/get_current_user', to: 'sessions#get_current_user'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :users, only: [:create]
 end
