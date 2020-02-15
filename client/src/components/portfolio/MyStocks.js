c2b1ad12729bbef3a58867991419e8bf8e5cf63f
Adam Sarli <adam.sarli@gmail.com>
2020-02-15 18:07:33 -0500

    current stocks in portfolio render

--- client/src/components/portfolio/MyStocks.js
+++ client/src/components/portfolio/MyStocks.js
@@ -1,29 +1,30 @@
 import React from 'react'
 import './css/myStocks.css'
+import { connect } from 'react-redux'
 
-const MyStocks = () => {
+const MyStocks = ({ stocks }) => {
   return (
     <div className="mystocks-wrapper">
-      <div className="stock-line">
-        <div className="shares">{`AAPL - 6 Shares`}</div>
-        <div className="value">{`$2043.09`}</div>
-      </div>
-
-      <div className="stock-separator"></div>
-
-      <div className="stock-line">
-        <div className="shares">{`AAPL - 6 Shares`}</div>
-        <div className="value">{`$2043.09`}</div>
-      </div>
-
-      <div className="stock-separator"></div>
-
-      <div className="stock-line">
-        <div className="shares">{`AAPL - 6 Shares`}</div>
-        <div className="value">{`$2043.09`}</div>
-      </div>
+      {stocks.allIds.map(stockId => {
+        const stock = stocks.byId[stockId]
+        return (
+          <div className="stock-wrapper" key={stockId}>
+            <div className="stock-line">
+              <div className="shares">{`${stock.symbol} - ${stock.shares} Shares`}</div>
+              <div className="value">{`$2043.09`}</div>
+            </div>
+            <div className="stock-separator"></div>
+          </div>
+        )
+      })}
     </div>
   )
 }
 
-export default MyStocks
+const mapStateToProps = state => {
+  return {
+    stocks: state.stocks
+  }
+}
+
+export default connect(mapStateToProps, null)(MyStocks)
