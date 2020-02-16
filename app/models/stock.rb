7061919ed2056e9adf39d7bf29f9db132230f604
Adam Sarli <adam.sarli@gmail.com>
2020-02-15 22:20:08 -0500

    Stock#.with_prices grabs current quotes for each stock the user owns

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -12,11 +12,15 @@ class Stock < ApplicationRecord
     end
   end
 
-  #https://cloud.iexapis.com/v1
-
   def self.with_prices(stocks)
+    resp = Faraday.get("https://cloud.iexapis.com/stable/stock/market/batch") do |req|
+      req.params['symbols'] = stocks.map{|stock| stock.symbol}.join(",")
+      req.params['types'] = 'quote'
+      req.params['token'] = ENV['IEX_KEY']
+    end
+    resp = JSON.parse(resp.body)
+    stocks.each{ |stock| stock.update(current_price: resp[stock.symbol]["quote"]["latestPrice"]) }
     stocks
-    #stocks.each{|stock| stock.update(current_price: Stock.lookup_price(stock.symbol))}
   end
 
 end
